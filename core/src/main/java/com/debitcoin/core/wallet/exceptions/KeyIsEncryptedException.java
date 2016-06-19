1f236a279e0512176c422c47fc2af00029a227dc
Debitcoin <debitcoinxyz@gmail.com>
2016-06-18 23:29:27 -0400

    core

---
+++ core/src/main/java/com/debitcoin/core/wallet/exceptions/KeyIsEncryptedException.java
@@ -0,0 +1,10 @@
+package com.debitcoin.core.wallet.exceptions;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class KeyIsEncryptedException extends Exception {
+    public KeyIsEncryptedException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ core/src/main/java/com/debitcoin/core/wallet/exceptions/NoSuchPocketException.java
@@ -0,0 +1,10 @@
+package com.debitcoin.core.wallet.exceptions;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class NoSuchPocketException extends Exception {
+    public NoSuchPocketException(String message) {
+        super(message);
+    }
+}
